b2d5e6f76eafe0a2629356e691e7f8d2b73bf913
Haotian <haotianzh@uconn.edu>
2025-11-18 08:32:25 -0500

    scistreecna first commit

--- build/lib/scistreecna/__init__.py
+++
@@ -1,8 +0,0 @@
-from . import util, external, simulate
-from .cn_estimate import *
-from .scistreecna import *
-from .topological_sort import *
-from .transition_solver import *
-
-
-__version__ = "0.1.0"  # first version
