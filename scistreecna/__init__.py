7be8118d9f041386e9bd2dde2bce32b74a322c4f
Haotian <haotianzh@uconn.edu>
2025-11-19 22:04:00 -0500

    fix segfault

--- scistreecna/__init__.py
+++ scistreecna/__init__.py
@@ -1,3 +1,11 @@
+import os 
+# avoid segfault when using linear solver
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+
 from .cn_estimate import CNEstimator
 from .scistreecna import (
 	NodeBatchLoader,
@@ -11,6 +19,7 @@ from .scistreecna import (
 )
 from .topological_sort import batch_topological_sort, topological_sort
 from .transition_solver import TransitionProbability
+
 
 
 __version__ = "0.1.0"  # first version
